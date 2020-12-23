b09883a37cec3e7f14b47b2b6c1c1048a2bda0e1
Scott <sbbartels@gmail.com>
2020-12-23 13:44:04 -0800

    Completed MyDataShell class.

--- MyFirstDataShell.py
+++ MyFirstDataShell.py
@@ -4,6 +4,14 @@ inventory_management = pd.read_csv('inventory.csv')
 
 class MyDataShell:
     
-    def __init__(self, filepath)
+    def __init__(self, filepath):
         self.filepath = filepath
         self.data_as_csv = pd.read_csv('inventory')
+
+    def get_status(self):
+        return self.data_as_csv.describe()
+
+inventory_management_stats = MyDataShell('inventory.csv')
+
+print(inventory_management)
+print(inventory_management_stats)
